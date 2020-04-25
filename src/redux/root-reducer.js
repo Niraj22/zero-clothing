ccc7219fc75036ba91cc51f22bf865e910c5b7ef
Niraj Budhathoki <nirajbudhathoki3@gmail.com>
2020-04-25 20:44:07 +0545

    added directory reducer and applied to directory component

--- src/redux/root-reducer.js
+++ src/redux/root-reducer.js
@@ -1,10 +1,10 @@
-import { combineReducers } from 'redux';
-import { persistReducer } from 'redux-persist';
-import storage from 'redux-persist/lib/storage';
+import { combineReducers } from 'redux'
+import { persistReducer } from 'redux-persist'
+import storage from 'redux-persist/lib/storage'
 
-import userReducer from './user/user.reducer';
-import cartReducer from './cart/cart.reducer';
-
+import userReducer from './user/user.reducer'
+import cartReducer from './cart/cart.reducer'
+import directoryReducer from './directory/directory.reducer'
 const persistConfig = {
   key: 'root',
   storage,
@@ -13,7 +13,8 @@ const persistConfig = {
 
 const rootReducer = combineReducers({
   user: userReducer,
-  cart: cartReducer
+  cart: cartReducer,
+  directory: directoryReducer
 });
 
-export default persistReducer(persistConfig, rootReducer);
+export default persistReducer(persistConfig, rootReducer)
--- src/redux/store.js
+++ src/redux/store.js
@@ -5,7 +5,7 @@ import logger from 'redux-logger';
 import rootReducer from './root-reducer';
 
 const middlewares = [logger];
-
+ 
 export const store = createStore(rootReducer, applyMiddleware(...middlewares));
 
-export const persistor = persistStore(store);
+export const persistor = persistStore(store); 
